48159bb051dc01caad4bf5f78eb84692143cfee2
wvfeders <wvfedders@frontier.com>
2017-11-30 10:26:18 -0500

    runs with any number of students

--- jimsrandomizer.rb
+++ jimsrandomizer.rb
@@ -1,12 +1,16 @@
 #jimsrandomizer
 def assign_pairs (student_names) #function that that receives an aray as an argument
 	shuffled_students = student_names.shuffle#creates a new array that is a shuffled version of the array in the argument
-	pair = shuffled_students.pop(2)
-	p pair 
-	p shuffled_students
-end
+	p shuffled_students.count #counts the number os students as a check
+	
+	while  shuffled_students.count > 3 do #starts a loop that runs as long as the array contains more than 3 names
+	pair = shuffled_students.pop(2).to_a #pulls out the last two names from the array and assigns them to pair
+	p pair #prints the pair
+	end #loop ends when the count of names is 3 or less
 
-assign_pairs(["Christi", "Doug", "Tyler", "Chris", "Jim"]) #run the function
+p shuffled_students #prints the remaining 2 or 3 students
+end
+assign_pairs(["Christi", "Doug", "Tyler", "Chris", "Jim", "Bob", "Barb", "Anna", "Rachel", "Emily", "Tricia"]) #run the function
 
 
 #pair = shuffled_students.each_slice(2).to_a #create a new array that holds arrays of 2,2 and 1 names
